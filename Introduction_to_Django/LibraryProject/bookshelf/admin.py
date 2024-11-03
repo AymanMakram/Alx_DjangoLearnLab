ca4527de1dd6111893d63e1d2c0e84717a4e2c56
Ayman <aymanmmokhtar@gmail.com>
2024-11-03 23:13:21 +0200

    3rd create admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,3 +4,7 @@ from .models import Book
 # Register the Book model with the admin site
 admin.site.register(Book)
 admin.ModelAdmin
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
