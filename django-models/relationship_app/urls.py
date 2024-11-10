3aa5169bdd344cbcbab16c75b61f11a38e0a8251
Ayman <aymanmmokhtar@gmail.com>
2024-11-10 20:48:05 +0200

    4th Django Views and URL Configuration

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,5 +3,5 @@ from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('', list_books, name='list_books'),  # Function-based view URL
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view URL
+    path('library/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view URL
 ]
