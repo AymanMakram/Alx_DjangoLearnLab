b75df5e148e187bf5999e5ffb52ad05f8b3c2559
Ayman <aymanmmokhtar@gmail.com>
2024-11-10 21:50:03 +0200

    6th Django Views and URL Configuration

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('', list_books, name='list_books'),  # Function-based view URL
     path('library/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view URL
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
+
+
+
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
@@ -14,6 +17,25 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+# relationship_app/views.py
+
+
+
+
+# Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            return redirect('home')  # Redirect to the homepage after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+# You don't need to write views for login and logout, as Django provides them
+# via LoginView and LogoutView.
 
 
     
