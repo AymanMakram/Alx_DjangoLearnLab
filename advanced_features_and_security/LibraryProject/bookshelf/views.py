7d65223d18fe7dc3f1225fbc87008bee89bd5dc3
Ayman <aymanmmokhtar@gmail.com>
2024-11-17 21:09:15 +0200

    2nd Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
+@permission_required('bookshelf.CustomUser', raise_exception= True)
+def SayHello(request):
+    book_list = list('')
+    return HttpResponse('Hello Worled')
+    
