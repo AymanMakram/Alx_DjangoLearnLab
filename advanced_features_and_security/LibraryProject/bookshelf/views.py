9ce2f02f4fd9839e758d55c87d061cbc231c0a6a
Ayman <aymanmmokhtar@gmail.com>
2024-11-23 23:11:13 +0200

    Building Your First API Endpoint with Django REST Framework

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -7,6 +7,6 @@ from django.contrib.auth.decorators import permission_required
 # Create your views here.
 @permission_required('bookshelf.CustomUser', raise_exception= True)
 def SayHello(request):
-    book_list = list('')
+    book_list = ['']
     return HttpResponse('Hello Worled')
     
