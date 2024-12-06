6b1f992b37557fdb3ddf7aac0d549b0400a4020b
Ayman <aymanmmokhtar@gmail.com>
2024-12-07 00:06:29 +0200

    Implementing the Blog's User Authentication System

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return self.title
+
+
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+# Registration View
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            login(request, form.instance)
+            return redirect('profile')  # Redirect to profile page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+# Profile View
+@login_required
+def profile(request):
+    if request.method == "POST":
+        user = request.user
+        user.email = request.POST.get('email')
+        user.save()
+        return redirect('profile')  # Redirect back to profile after update
+    return render(request, 'registration/profile.html')
