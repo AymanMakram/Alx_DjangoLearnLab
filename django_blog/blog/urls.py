befc4476d52e2287c794892d64de264abc652de3
Ayman <aymanmmokhtar@gmail.com>
2024-12-07 00:10:21 +0200

    User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
