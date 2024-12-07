666611e385786ab9a1916d8ee0d741e03b651985
Ayman <aymanmmokhtar@gmail.com>
2024-12-07 16:05:06 +0200

    4th Adding Comment Functionality to Blog Posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from .views import post_detail, CommentUpdateView, CommentDeleteView
+from .views import post_detail, CommentUpdateView, CommentDeleteView, CommentCreateView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
@@ -14,7 +14,9 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('posts/<int:pk>/', post_detail, name='post-detail'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/', post_detail, name='post-detail'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-new'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
+
